5f5a91c70e53fb1211d9d322eeac60e2b1915ce4
benjamesmurray <benjamesmurray@gmail.com>
2019-12-23 16:18:19 +0000

    update home and index
    update index with directory function

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php include_once("home.html")	
 
-?>
+
+echo "Here are our files";
+$path = ".";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='$path/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
