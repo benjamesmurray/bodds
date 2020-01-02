fed66b80156a371c0214e7c6833a355dd6e6ee71
benjamesmurray <benjamesmurray@gmail.com>
2020-01-02 09:10:04 +0000

    add test sets by folder

--- index.php
+++ index.php
@@ -2,13 +2,44 @@
 ?> 
 
 <?php
-echo "";
-$path = "/app/testdata";
+echo "Test data set 1
+";
+$path = "/app/testdata/group_1";
 $dh = opendir($path);
 $i=1;
 while (($file = readdir($dh)) !== false) {
     if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
-        echo "<a href='/testdata/$file'>$file</a><br /><br />";
+        echo "<a href='/testdata//group_1/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "Test data set 2
+";
+$path = "/app/testdata/group_2";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata//group_2/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "Test data set 3
+";
+$path = "/app/testdata/group_3";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata//group_3/$file'>$file</a><br /><br />";
         $i++;
     }
 }
